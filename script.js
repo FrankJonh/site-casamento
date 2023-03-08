1289522043c22f1cc76e524b1e74594bc6f91a62
Frank jONH <frank_11jonh@hotmail.com>
2023-03-08 16:46:11 -0300

    Menu com Scroll

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ const btnMenu = document.getElementById('button-menu');
 const navMenu = document.getElementById('menu');
 const menuFixed = document.getElementById('container-menu');
 
+window.addEventListener('scroll', activeScroll);
+
+function activeScroll(){
+    menuFixed.classList.toggle('ativo', scrollY >450);
+}
+
 btnMenu.addEventListener('click', ()=> {
     btnMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-
-function activeScroll(){
-    menuFixed.classList.toggle('ativo', scrollY > 390);
-}
-
-window.addEventListener('scroll', activeScroll);
-
-
-const posicoes = menuFixed.getBoundingClientRect();
-
-console.log(posicoes);
