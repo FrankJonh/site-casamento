68d3eb9f935874f985bcbf54233543597baa3dc1
Frank jONH <frank_11jonh@hotmail.com>
2023-04-12 22:47:32 -0300

    atualizacao do menu responsivo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnMenu = document.getElementById('button-menu');
 const navMenu = document.getElementById('menu');
 const menuFixed = document.getElementById('container-menu');
+const nossoNome = document.querySelector('.nosso-nome');
 
 window.addEventListener('scroll', activeScroll);
 
@@ -11,4 +12,13 @@ function activeScroll(){
 btnMenu.addEventListener('click', ()=> {
     btnMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
+
 })
+
+function closeMenu() {
+    navMenu.classList.remove('active');
+    btnMenu.classList.remove('active');
+    
+}
+
+
